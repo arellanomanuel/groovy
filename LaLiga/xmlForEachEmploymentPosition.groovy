0b23d191f9a591f177a5e79927ca985f2d8bf195
arellano <113515306+arellanomanuel@users.noreply.github.com>
2023-06-07 14:07:24 +0200

    Update xmlForEachEmploymentPosition.groovy

--- LaLiga/xmlForEachEmploymentPosition.groovy
+++ LaLiga/xmlForEachEmploymentPosition.groovy
@@ -18,24 +18,57 @@ def Message processData(Message message) {
             parsedXml.CompoundEmployee.each { emp ->
                  emp.person.employment_information.each { empInf ->
                  EMPLEADO{
-                    CODEMP(empInf.job_information.company.text())
-                    //CODTRABA nombre???? crear 2 campos misma info
-                    //no nesta en el excel CODTRABA_INTERNO -> user_id de employmentinformation
-                    //campo si es principal o no lectura mapa
                     CODTRABA(emp.person.person_id_external.text())
-                    //este o user_di
+                    //este o user_id
                     CODTRABA_INTERNO(empInf.assignmentIdExternal.text())
                     N_MATRICULA(emp.person.person_id_external.text())
-                    GRUPOTC(empInf.job_information.custom_string4.text())
-                    TRIBUTA_REG_GEN(empInf.job_information.custom_string8.text())
-                    TRIBUTA_REG_FORAL(empInf.job_information.custom_string8.text())
-                    REGIMEN_FORAL(empInf.job_information.custom_string8.text())
-                    TRIBUTA_216(empInf.job_information.custom_string9.text())
-                    APELLIDOS(emp.person.personal_information.last_name.text())
-                    NOMBRE(emp.person.personal_information.first_name.text())
+
+                   empInf.job_information.each { jobInfo ->
+                        if (jobInfo.end_date.text()=="9999-12-31") {                    
+                            CODEMP(jobInfo.company.text())
+                            GRUPOTC(jobInfo.custom_string4.text())
+                            TRIBUTA_REG_GEN(jobInfo.custom_string8.text())
+                            TRIBUTA_REG_FORAL(jobInfo.custom_string8.text())
+                            REGIMEN_FORAL(jobInfo.custom_string8.text())
+                            TRIBUTA_216(jobInfo.custom_string9.text())
+                            CECO(jobInfo.department.text())
+                            //TO DO: O2cecos mismo nombre
+                            CECO(jobInfo.custom_string101.text())
+                            
+                            CODIGO_CATEGORIA(jobInfo.job_code.text())
+                            PUESTO(jobInfo.job_title.text())
+                           
+
+                            TIPO_CONTRATO(jobInfo.custom_string10.text())
+                            JORNADA_DIARIA(jobInfo.custom_double1.text())
+                            TIPO_TRABAJADOR(jobInfo.employee_class.text())
+                            GRUPO_COTIZACION(jobInfo.custom_string3.text())
+                            SITU_ESP_COTIZACION(jobInfo.custom_string6.text())
+                            COD_CENTRO(jobInfo.custom_string6.text())
+                            CAUSA(jobInfo.event_reason.text())
+                           
+                            
+                            NOMBRE_DEPARTAMENTO(jobInfo.department.text())
+                            CONVENIO_VACACIONES(jobInfo.custom_string12.text())
+                            FECHA_FIN_CONTRATO(jobInfo.contract_end_date.text())
+                        }
+                    }
+
+
+                    emp.person.personal_information.each { perInfo ->
+                        if (perInfo.end_date.text()=="9999-12-31") {
+                            APELLIDOS(perInfo.last_name.text())
+                            NOMBRE(perInfo.first_name.text())
+                            SEXO(perInfo.gender.text())
+                            GRADO_DISCAPACIDAD(perInfo.personal_information_esp.custom_string1.text())
+                            AYUDA_DESPLAZAMIENTO(perInfo.personal_information_esp.custom_string2.text())
+                            SITUACION_FISCAL(perInfo.marital_status.text())
+                            NIF_CONYUGE(perInfo.personal_information_esp.genericString1.text())
+                        }
+                    }
                     //TO DO: solo del que tiene el tipo home
                     emp.person.address_information.each { addInfo ->
-                        if (addInfo.address_type.text() == "home") {
+                        if (addInfo.address_type.text() == "home" && addInfo.end_date.text()=="9999-12-31") {
                             DIRECCION(addInfo.address1.text())
                             MUNICIPIO(addInfo.city.text())
                             PROVINCIA(addInfo.state.text())
@@ -60,38 +93,28 @@ def Message processData(Message message) {
                     }
                     //TO DO: sin ejemplos de empleados con national_id_card
                     //TO DO: if N_AFILIACION == NAF ????
-                    N_AFILIACION(emp.person.national_id_card.card_type.text())
-                    DOCUMENTO_IDENTIFI(emp.person.national_id_card.national_id.text())
-                    //TO DO: if N_AFILIACION == COD_DOCUMENTO_IDENTIF ????
-                    COD_DOCUMENTO_IDENTIF(emp.person.national_id_card.card_type.text())
-
+                    emp.person.national_id_card.each { idCard ->
+                        if (idCard.card_type.text() == "NAF") {
+                            COD_DOCUMENTO_IDENTIF(idCard.card_type.text())
+                            N_AFILIACION(idCard.national_id.text())
+                        }
+                        if (idCard.card_type.text() == "DNI") {
+                            COD_DOCUMENTO_IDENTIF(idCard.card_type.text())
+                            DOCUMENTO_IDENTIFI(idCard.national_id.text())
+                        }
+                            //TO DO: if N_AFILIACION == COD_DOCUMENTO_IDENTIF ????
+                        
+                    }
                     FECHA_NACIMIENTO(emp.person.date_of_birth.text())
-                    CECO(empInf.job_information.department.text())
-                    //TO DO: O2cecos mismo nombre
-                    CECO(empInf.job_information.custom_string101.text())
-                    SEXO(emp.person.personal_information.gender.text())
-                    CODIGO_CATEGORIA(empInf.job_information.job_code.text())
-                    PUESTO(empInf.job_information.job_title.text())
-                    //TO DO: sin ejemplos de empleados con iban
+                    //TO DO: sin ejemplos de empleados con iban me falta PaymentInformationV3
                     IBAN(empInf.PaymentInformationV3.PaymentInformationDetailV3.iban.text())
                     CUENTA_BANCARIA(empInf.PaymentInformationV3.PaymentInformationDetailV3.accountNumber.text())
 
-                    TIPO_CONTRATO(empInf.job_information.custom_string10.text())
-                    JORNADA_DIARIA(empInf.job_information.custom_double1.text())
                     FECHA_ALTA(empInf.start_date.text())
                     FECHA_ANTIGUEDAD(empInf.seniorityDate.text())
-                    TIPO_TRABAJADOR(empInf.job_information.employee_class.text())
-                    GRUPO_COTIZACION(empInf.job_information.custom_string3.text())
-                    SITU_ESP_COTIZACION(empInf.job_information.custom_string6.text())
-                    COD_CENTRO(empInf.job_information.custom_string6.text())
                     FECHA_BAJA(empInf.end_date.text())
-                    CAUSA(empInf.job_information.event_reason.text())
-                    //TO DO:hPath["person/employment_information/compensation_information/paycompensation_recurring/paycompvalue pay_component = 0YA/"]
-                    SALARIO_BRUTO_ANUAL(empInf.compensation_information.paycompensation_recurring.paycompvalue.text())
+                   
                     
-                    NOMBRE_DEPARTAMENTO(empInf.job_information.department.text())
-                    CONVENIO_VACACIONES(empInf.job_information.custom_string12.text())
-
                     //TO DO: email solo business
                     emp.person.email_information.each { emailInfo ->
                         if (emailInfo.email_taype.text() == "B") {
@@ -99,22 +122,31 @@ def Message processData(Message message) {
                         }
                     }
                     
-                    GRADO_DISCAPACIDAD(emp.person.personal_information.personal_information_esp.custom_string1.text())
-                    AYUDA_DESPLAZAMIENTO(emp.person.personal_information.personal_information_esp.custom_string2.text())
+                 
                     //TO DO: HIJOS Y ASCENDENTES
 
-                    SITUACION_FISCAL(emp.person.personal_information.marital_status.text())
-                    NIF_CONYUGE(emp.person.personal_information.personal_information_esp.genericString1.text())
-                    VARIABLE_ANIO_ANT(empInf.compensation_information.custom_string46.text())
-                    VARIABLE_PREVISTA(empInf.compensation_information.custom_string1.text())
-                    FECHA_FIN_CONTRATO(empInf.job_information.contract_end_date.text())
-
-                    //TO DO: con q se compara sin ejemplos
-
-                    SEGURO_MEDICO(empInf.compensation_information.paycompensation_recurring.paycompvalue.text())
-                    TELETRABAJO(empInf.compensation_information.paycompensation_recurring.paycompvalue.text())
-                    PORCEN_VARIABLE(empInf.compensation_information.paycompensation_recurring.paycompvalue.text())
-
+                    
+                    empInf.compensation_information.each { comInfo ->
+                        if (comInfo.end_date.text()=="9999-12-31") {
+                            VARIABLE_ANIO_ANT(comInfo.custom_string46.text())
+                            VARIABLE_PREVISTA(comInfo.custom_string1.text())
+                            comInfo.paycompensation_recurring.each { pComInfo ->
+                                if (pComInfo.end_date.text()=="9999-12-31" && pComInfo.pay_component.text()=="0YA") {
+                                    //TO DO:hPath["person/employment_information/compensation_information/paycompensation_recurring/paycompvalue pay_component = 0YA/"]
+                                    SALARIO_BRUTO_ANUAL(pComInfo.paycompvalue.text())
+                                }
+                                if (pComInfo.end_date.text()=="9999-12-31" && pComInfo.pay_component.text()=="4") {    
+                                    SEGURO_MEDICO(pComInfo.paycompvalue.text())
+                                }
+                                if (pComInfo.end_date.text()=="9999-12-31" && pComInfo.pay_component.text()=="011") {
+                                    TELETRABAJO(pComInfo.paycompvalue.text())
+                                }
+                                if (pComInfo.end_date.text()=="9999-12-31" && pComInfo.pay_component.text()=="PorcVariable") {
+                                    PORCEN_VARIABLE(pComInfo.paycompvalue.text())}
+                            }
+                            
+                        }
+                    }
                     // Agregar más propiedades aquí según sea necesario
                 
             }
